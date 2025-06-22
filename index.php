0184e728fbaec37b3ce87be2a02c357e8adb059c
Owen Liangco <owenliangco21@gmail.com>
2025-06-22 16:57:44 +0800

    feat(index): added code in index to check connectivity

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h2>Database Connection Checker</h2>";
+
+include_once __DIR__ . '/handlers/mongodbChecker.handler.php';
+include_once __DIR__ . '/handlers/postgreChecker.handler.php';
+?>
