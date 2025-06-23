326973e6e3ff5ba703b39194f6ae80e7011ef156
Owen Liangco <owenliangco21@gmail.com>
2025-06-23 11:38:35 +0800

    feat(bootstrap): modified the paths

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,8 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
+define('UTILS_PATH', BASE_PATH . '/utils/');
+define('VENDOR_PATH', BASE_PATH . '/vendors/');
+define('HANDLERS_PATH', BASE_PATH . '/handlers/');
 
 chdir(BASE_PATH);
+
