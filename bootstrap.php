779b7613eefb2a32b788a952056ca2344c22473e
Owen Liangco <owenliangco21@gmail.com>
2025-07-06 13:20:26 +0800

    feat(bootstrap): added the seeder key set

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,7 @@ define('BASE_PATH', realpath(__DIR__));
 define('UTILS_PATH', BASE_PATH . '/utils/');
 define('VENDOR_PATH', BASE_PATH . '/vendors/');
 define('HANDLERS_PATH', BASE_PATH . '/handlers/');
+define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/');
 
 chdir(BASE_PATH);
 
