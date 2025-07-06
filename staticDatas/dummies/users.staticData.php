09a523c18984f4b10f0d96f35953feab8ba9457a
Owen Liangco <owenliangco21@gmail.com>
2025-07-06 13:21:25 +0800

    feat(staticData): added a new static data for users

---
+++ staticDatas/dummies/users.staticData.php
@@ -0,0 +1,20 @@
+<?php
+// Columns: id, username, first_name, last_name, password, role
+return [
+    [
+        'username'   => 'john.smith',
+        'email'      => 'john.smith@example.com',
+        'first_name' => 'John',
+        'last_name'  => 'Smith',
+        'password'   => 'p@ssW0rd1234',
+        'role'       => 'designer',
+    ],
+    [
+        'username'   => 'jane.doe',
+        'email'      => 'jane.doe@example.com',
+        'first_name' => 'Jane',
+        'last_name'  => 'Doe',
+        'password'   => 'secret123',
+        'role'       => 'developer',
+    ],
+];
